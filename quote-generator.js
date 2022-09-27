f2d7b5115d80bf121a6eff5d3b22d5372d1697a2
Day Vazquez <dayvazquez@protonmail.com>
2022-09-27 22:43:28 +0200

    Initial commit

---
+++ quote-generator.js
@@ -0,0 +1,9 @@
+// Random quote generator program in JavaScript using an API
+
+fetch("https://type.fit/api/quotes")
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+  });
