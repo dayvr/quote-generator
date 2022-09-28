be8e5ea4df89c98f7fc5d3d9393f763051b9fc60
Day Vazquez <dayvazquez@protonmail.com>
2022-09-28 23:19:09 +0200

    Update file

--- quote-generator.js
+++ quote-generator.js
@@ -7,3 +7,15 @@ fetch("https://type.fit/api/quotes")
   .then(function (data) {
     console.log(data);
   });
+
+let quote = document.getElementById("text");
+let author = document.getElementById("author");
+let btn = document.getElementById("new-quote");
+
+function randomQuote() {
+  let random = Math.floor(Math.random() * 1643);
+  quote.innerHTML = data[random].text;
+  author.innerHTML = data[random].author;
+}
+
+btn.addEventListener("click", randomQuote);
